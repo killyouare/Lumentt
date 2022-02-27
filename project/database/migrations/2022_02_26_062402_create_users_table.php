47e5ac9ffeee9373e96a617b15955e1931876f04
killyouare <ya.ilyaku@gmail.com>
2022-02-27 13:20:10 +0700

    refactor paths

---
+++ project/database/migrations/2022_02_26_062402_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 256);
+            $table->string('password', 256);
+            $table->string('phone', 16);
+            $table->string('last_name', 64);
+            $table->string('first_name', 64);
+            $table->boolean('is_admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Users');
+    }
+};
